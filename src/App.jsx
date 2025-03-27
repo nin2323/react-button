953ae308869cf647c48c06f887d5e2d20020bd77
Juan <juansobraoperez@gmail.com>
2025-03-27 12:57:07 +0100

    boton enviar tarea con input

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,30 @@ import './App.css'
 
 export const App = () => {
   const [tasksArray, setTasksArray] = useState([]);
+  const [taskText, setTaskText] = useState('');
 
   const handleAddTask = () => {
     setTasksArray([...tasksArray, `Tarea ${tasksArray.length + 1}`]);
+  };
+
+  const handleSendTask = () => {
+    if (taskText.trim() !== '') { 
+      setTasksArray([...tasksArray, taskText]);
+      setTaskText(''); 
+    }
   };
 
   return (
     <>
       <button onClick={handleAddTask}>Añadir tarea</button>
       <p>Tarea creadas {tasksArray.length}</p>
-      <div>{tasksArray}</div>
+      <input type="text" placeholder='Escribir tarea' value={taskText} onChange={(e) => {setTaskText(e.target.value)}} />
+      <button className='button-text' onClick={handleSendTask}>Enviar tarea</button>
+      <p>
+        {tasksArray.map((task, index) => (
+          <span key={index}>{task} <br /></span>
+        ))}
+      </p>
     </>
   )
 }
